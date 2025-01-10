bb2fa9e0dba60c68c403b0e60d4b008730b5523c
Brendo-will <116374076+Brendo-will@users.noreply.github.com>
2025-01-09 23:23:09 -0300

    Update NBA.py

--- NBA.py
+++ NBA.py
@@ -9,7 +9,8 @@ import plotly.graph_objects as go
 DEFAULT_IMAGE = "https://via.placeholder.com/150"
 IMAGES_DIR = "fotos"
 DEFAULT_LOGO = "https://via.placeholder.com/150"
-LOGOS_DIR = "Logos"
+LOGOS_DIR = os.path.join(BASE_DIR, "Logos")
+
 
 # Função para obter dados dos times via API
 @st.cache_data
